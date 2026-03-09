8dd9d6ce67aed6c2955f93312368301e05b823e2
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2026-03-10 00:45:42 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,23 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  const navigate = useNavigate();
 
-  const { loginWithRedirect } = useAuth0();
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [isAuthenticated, navigate]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="h-screen flex items-center justify-center">
-
       <div className="bg-slate-800 p-10 rounded-xl text-center w-96">
 
         <h1 className="text-3xl font-bold mb-6">
